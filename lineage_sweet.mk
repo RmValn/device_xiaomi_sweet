f545e817bd114aa953d4a8609f2fc00fd8b93f90
mikoker <mikolaj.kolata@gmail.com>
2025-06-30 23:14:58 +0200

    sweet: initialize Evolution-X 11.x
    Change-Id: I702462ba3df9b4737b5910acccfbf6e9117b1d9a

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -15,6 +15,14 @@ $(call inherit-product, device/xiaomi/sweet/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Evolution X
+TARGET_INCLUDE_ACCORD := false
+TARGET_ENABLE_BLUR := true
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+TARGET_SUPPORTS_64_BIT_APPS := true
+BUILD_BCR := true
+BYPASS_CHARGE_SUPPORTED := true
+
 PRODUCT_NAME := lineage_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
